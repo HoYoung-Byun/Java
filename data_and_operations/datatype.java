89dccde0da677d86a8ca6947c4a7fa0a0b6a8eae
HoYoung-Byun <anthony0102@naver.com>
2022-02-15 23:24:05 +0900

    first commit

---
+++ data_and_operations/datatype.java
@@ -0,0 +1,8 @@
+public class datatype{
+	public static void main(String[] args) {
+		System.out.println(6); //Number
+		System.out.println("six"); //String
+		System.out.println("6"); //String
+		System.out.println();
+	}
+}
