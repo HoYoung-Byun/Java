a82647afa69debdfda2ba2f44839a7d003da40da
HoYoung-Byun <anthony0102@naver.com>
2022-02-25 14:20:21 +0900

    2022-02-25-14:20

---
+++ Programming/Ok_JavaGoInHome.java
@@ -0,0 +1,24 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Security;
+import org.opentutorials.iot.Lighting;
+
+
+public class Ok_JavaGoInHome {
+
+	public static void main(String[] args) {
+		
+		// Elevator call
+		Elevator myElevator = new Elevator("Java APT 507");
+		myElevator.callForUp(1);
+		
+		// Security off
+		
+		
+		
+		// Light on
+		
+		
+
+	}
+
+}
